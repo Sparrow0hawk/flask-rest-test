40de5051f34395ff743c53eb148ddf461c11e248
Sparrow0hawk <alexjcoleman@gmail.com>
2023-10-29 19:47:29 +0000

    Group sections of create_app by purpose

--- src/flask_rest_test/__init__.py
+++ src/flask_rest_test/__init__.py
@@ -8,14 +8,17 @@ from flask_rest_test.extensions import app_db
 def create_app(test_config=None) -> Flask:
     app = Flask(__name__)
 
+    # app config
     app.config.from_object("flask_rest_test.config.Config")
     app.config.from_prefixed_env()
-
-    app_db.init_app(app)
-
     if test_config:
         app.config.from_mapping(test_config)
 
+    # extensions
+    app_db.init_app(app)
+
+    # app routes
+    # TODO: refactor into blueprints
     @app.route("/")
     def index() -> str:
         with app_db.engine.connect() as conn:
@@ -39,6 +42,7 @@ def create_app(test_config=None) -> Flask:
         except Exception as e:
             return Response(response=f"Something went wrong: {e.__doc__}\n", status=400)
 
+    # database data initialisation
     app_db.create_database()
 
     return app
